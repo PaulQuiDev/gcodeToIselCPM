19c0ba61a004db0f249b630a9c6d60389a4ac782
PaulQuiDev <80120197+PaulQuiDev@users.noreply.github.com>
2024-05-17 14:54:02 +0200

    Fonctionnelle
    bug fix which meant that a move in one direction was not taken into account

--- ClasseCNC.py
+++ ClasseCNC.py
@@ -10,10 +10,18 @@ class CNC:
         
         self.ser = ser
         self.eta = False
+        # ===== ===== = == = = === Posisiont globale
         self.x = 0  
         self.y = 0
         self.z = 0
-        self.speed = 100
+        # ==== = = = = = = = = 
+        self.speed = 100 # vitesse pour le G0 déplacement rapide 
+
+        # ------------------------------------ Pausition 0 locale
+        self.x0 = 0  
+        self.y0 = 0
+        self.z0 = 0
+
 
 #Note : position minuscule = position lengage machine , Maj = position en mm 
 
@@ -35,33 +43,82 @@ class CNC:
         for ligne in gcode:
             if ligne.find(';') == -1:
                 instru.append(ligne)
-
-        speed = 500
-        x , y , z = 0 , 0 , 0  #initilaliser a 0
+        
+        speed = 200
+        x , y , z = 0 , 0 , 0  #initilaliser au 0 locale 
+        minx , miny , maxx , maxy = 8000 ,6000,0,0
         ordre = []
         for i in instru:
             a = i.split(' ')
-            if len(a) > 2 and a[0][0] == 'G':  # commande de direction
+            if len(a) > 1 and a[0][0] == 'G':  # commande de direction
                 for truc in a:
                     if truc.find('X') != -1:
-                        x = int(float(truc.removeprefix('X')))
+                        x = float(truc.removeprefix('X'))
+                        #print('x :' , x , " machin=" , x*40)
                     if truc.find('Y') != -1:
-                        y = int(float(truc.removeprefix('Y')))
+                        y = float(truc.removeprefix('Y'))
+                        #print('y :', y , " machin=" , y*40)
                     if truc.find('Z') != -1:
-                        z = int(float(truc.removeprefix('Z')))
-                ordre.append('@0M ' + str(x*40) + "," + str(speed) + "," + str(y*40) + "," + str(speed) + "," + str(z*(-40)) + "," + str(speed) + "," + str(z*(-40)) +  "," + str(speed) + "\r")
+                        z = float(truc.removeprefix('Z'))
+                        #print('z :' , z, " machin=" , z*40)
+                    if truc.find('F') != -1:
+                        speed = int(float(truc.removeprefix('F')))
+                if (i[1] == '1' or i[2] == '1'): # suivent les versiont c'est G1 ou G01
+                    ordre.append(f"@0M {int(x*40 + self.x0)},{speed},{int(y*40 + self.y0)},{speed},{(int(z*40 - self.z0))},{speed},{(int(z*40 - self.z0))},{speed}\r")
+                    #self.afficher_instruction(f"@0M {int(x*40 + self.x0)},{speed},{int(y*40 + self.y0)},{speed},{(int(z*40 - self.z0))},{speed},{(int(z*40 - self.z0))},{speed}\r",ende='\n\n ')
+
+                elif (i[1] == '0' or i[2] == '0') : #c'est un G0 mouvement rapide 
+                    ordre.append(f"@0M {int(x*40 + self.x0)},{self.speed},{int(y*40 + self.y0)},{self.speed},{(int(z*40 - self.z0))},{self.speed},{(int(z*40 - self.z0))},{self.speed}\r")
+                    #self.afficher_instruction(f"@0M {int(x*40 + self.x0)},{self.speed},{int(y*40 + self.y0)},{self.speed},{(int(z*40 - self.z0))},{self.speed},{(int(z*40 - self.z0))},{self.speed}\r",ende='\n\n')
+                else :
+                    print('Commande Non Pris en compte ' , i)
+                posx = int(x*40 + self.x0)
+                posy = int(y*40 + self.y0 )
+
+                if  (abs(z*40) + abs(self.z0) > 4000): #les axes sont dans le positif et ne dépasses pas le Volument de l'imprimente
+                    print('Dépasse axe Z' , z)
+                    return []
+                if (posx > 8000 or posx < 0):
+                    print('hors max X')
+                    return []
+                if (posx > maxx): maxx = round( posx, -1)
+                if (posx < minx) : minx = round( posx, -1)
+
+                if (posy > maxy): maxy = round( posy, -1)
+                if (posy < miny) : miny = round( posy, -1)
+        #faire le dessin de la zone 
+        if (self.z0 > 200): retract = 10
+        else : retract = 0
+        if(minx != 8000 or miny != 6000 ):
+            maxx = int(maxx/40)
+            minx = int(minx/40)
+            maxy = int(maxy/40)
+            miny = int(miny/40)
+            self.allezA(int(self.x0/40),int(self.y0/40 ),(int(self.z0/40)- retract))
+            self.allezA(minx,miny,(int(self.z0/40)- retract))  # -5 pou eviter que la téte ne fortte 
+            self.allezA(minx,maxy,(int(self.z0/40)- retract))
+            self.allezA(maxx,maxy,(int(self.z0/40)- retract))
+            self.allezA(maxx, miny,(int(self.z0/40)- retract))
+            self.allezA(minx,miny,(int(self.z0/40)- retract))
         return ordre
 
     def initialisation_connexion(self) -> None:
         if (self.ser != None ):
             sorti = self._commander_("@07\r")
             print("Initialisation Connection: " , sorti )
+            self._commander_("@0B1,1\r")
+            self._commander_("@0B2,0\r")
+            self._commander_("@0B3,1\r")
+            self._commander_("@0B4,0\r")
+            self._commander_("@0B5,0\r")
+            self._commander_("@0B,64\r")
             if (sorti == "0"):
                 sorti= self._commander_("@0R7\r")
                 print("AutoHome :" , sorti )
                 if (sorti == "0"):
                     self.eta =True
                     self.x , self.y ,self.z = 0,0,0
+                
             else:
                 self.eta = False
         else:
@@ -77,7 +134,7 @@ class CNC:
 
     def afficher_instruction(self, instruction, ende="\n") -> None:
         instru = instruction.split(',')
-        print("X: {:.2f} Y: {:.2f} Z: {:.2f} speed: {:.2f}".format(float(instru[0][4:])/40, float(instru[2])/40, abs(float(instru[4]))/40, float(instru[2])/40), end=ende)
+        print("X: {:.2f} Y: {:.2f} Z: {:.2f} speed: {:.2f}".format(float(instru[0][4:])/40, float(instru[2])/40, float(instru[4])/40, float(instru[3])/40), end=ende)
 
     def envoyer_Posistion(self, instruction: str) -> str: # envoyer commede de position 
         if (self.eta == True):
@@ -114,10 +171,10 @@ class CNC:
             elif (Z < 0 or Z > 100):
                 return ("z en dehors du plateau")
             else:
-                self.envoyer_Posistion('@0M ' + str(X*40) + "," + str(self.speed) + "," + str(Y*40) + "," + str(self.speed) + "," + str(Z*(-40)) + "," + str(self.speed) + "," + str(Z*(-40)) +  "," + str(self.speed) + "\r")
+                return self.envoyer_Posistion(f"@0M {X*40},{self.speed},{Y*40},{self.speed},{-abs(Z*40)},{self.speed},{-abs(Z*40)},{self.speed}\r")
         else: print('machine non dispo')
 
-    def allezAMachine(self,x:int,y:int,z:int)-> str:
+    def allezAMachine(self,x:int,y:int,z:int)-> str: # problemme 
         if (self.eta == True ):
             if (x < 0 or x > 8000):
                 return ("x en dehors du plateau")
@@ -126,8 +183,14 @@ class CNC:
             elif (z < 0 or z > 4000):
                 return ("z en dehors du plateau")
             else:
-                self.envoyer_Posistion('@0M ' + str(x) + "," + str(self.speed) + "," + str(y) + "," + str(self.speed) + "," + str(z*(-1)) + "," + str(self.speed) + "," + str(z*(-1)) +  "," + str(self.speed) + "\r")
+                print(f"@0M {x},{self.speed},{y},{self.speed},{-abs(z)},{self.speed},{-abs(z)},{self.speed}\r")
+                return self.envoyer_Posistion(f"@0M {x},{self.speed},{y},{self.speed},{-abs(z)},{self.speed},{-abs(z)},{self.speed}\r")
         else: print('machine non dispo')
+
+    def SetLocal0(self) -> None:
+        self.x0 = self.x
+        self.y0 = self.y
+        self.z0 = self.z
 
     def AutoHome(self) -> str:
         return self.commande("@0R7\r")
@@ -174,13 +237,17 @@ class CNC:
                 #print("Machine non disponible ¯\_(ツ)_/¯")
                 return("/" + retour + "/ Erreur Machine non démarer")
             elif retour == "2" :  
-                return("/" + retour + "/ Erreur Coordonner Hors plateau")
+                return("/" + retour + "/ Erreur Coordonner Hors plateau ou emergency tsop")
             elif retour == "9":
                 return("/" + retour + "/ Erreur Alimentation couper")
             elif retour == "5":
                 return("/" + retour + "/ Erreur Bouton stop presser")
-            elif retour == "2":
-                return( retour + " Probleme Ordre non compris")
+            elif retour == "3":
+                return("/" + retour + "/ illegal number of axes")
+            elif retour == "4":
+                return("/" + retour + "/ axis not defind")
+            elif retour == "7":
+                return("/" + retour + "/ illegal parametre")
             else :
                 return ("/" + retour + "/ Erreur de....")
     
@@ -192,19 +259,20 @@ if __name__ == "__main__":
 
     briot.DefVitess(20)
 
-    briot.allezA(15,25,10)
+    briot.allezA(140,29,31)
+    briot.SetLocal0()
 
-    briot.AutoHome()
-
-    gcode = briot.lire_fichier_gcode("logo(2).gcode")
-    ordre = briot.generer_ordres(gcode)
-
+    gcode = briot.lire_fichier_gcode("Holder.nc")
+    instruction = briot.generer_ordres(gcode)
+    
+    
     briot.OutilsDemarer()
 
-    for i in ordre:
-        briot.afficher_instruction(i,ende = "\t")
+    for i in instruction:
+        briot.afficher_instruction(i, ende="\t")
         print(briot.envoyer_Posistion(i))
-    
+
+
     briot.OutilsStop()
 
     briot.deconnection()
