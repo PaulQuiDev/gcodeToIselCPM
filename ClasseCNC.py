51f21de38629936b1c9ece015dd8c30f32028753
PaulQuiDev <80120197+PaulQuiDev@users.noreply.github.com>
2024-05-24 09:23:28 +0200

    fix bug for move

--- ClasseCNC.py
+++ ClasseCNC.py
@@ -9,9 +9,9 @@ class CNC:
             ser = None
         
         self.ser = ser
-        self.eta = False
+        self.state = False
         # ===== ===== = == = = === Posisiont globale
-        self.x = 0  
+        self.x = 0 
         self.y = 0
         self.z = 0
         # ==== = = = = = = = = 
@@ -25,10 +25,10 @@ class CNC:
 
 #Note : position minuscule = position lengage machine , Maj = position en mm 
 
-    def lire_fichier_gcode(self, nom_fichier: str) -> list:
+    def Read_gcode(self, name_file: str) -> list:
         lignes_gcode = []
         try:
-            with open(nom_fichier, 'r') as fichier:
+            with open(name_file, 'r') as fichier:
                 for ligne in fichier:
                     # Supprimer les espaces et les sauts de ligne
                     ligne = ligne.strip()
@@ -38,7 +38,7 @@ class CNC:
             print("Le fichier spécifié n'existe pas.")
         return lignes_gcode
 
-    def generer_ordres(self, gcode : list) -> list:
+    def generate_order(self, gcode : list) -> list:
         instru = []
         for ligne in gcode:
             if ligne.find(';') == -1:
@@ -77,10 +77,13 @@ class CNC:
 
                 if  (abs(z*40) + abs(self.z0) > 4000): #les axes sont dans le positif et ne dépasses pas le Volument de l'imprimente
                     print('Dépasse axe Z' , z)
-                    return []
+                    return ['Dépasse axe Z']
                 if (posx > 8000 or posx < 0):
                     print('hors max X')
-                    return []
+                    return ['hors max X']
+                if (posy > 6000 or posy < 0): 
+                    print('hors max y')
+                    return['hors max y']
                 if (posx > maxx): maxx = round( posx, -1)
                 if (posx < minx) : minx = round( posx, -1)
 
@@ -94,15 +97,15 @@ class CNC:
             minx = int(minx/40)
             maxy = int(maxy/40)
             miny = int(miny/40)
-            self.allezA(int(self.x0/40),int(self.y0/40 ),(int(self.z0/40)- retract))
-            self.allezA(minx,miny,(int(self.z0/40)- retract))  # -5 pou eviter que la téte ne fortte 
-            self.allezA(minx,maxy,(int(self.z0/40)- retract))
-            self.allezA(maxx,maxy,(int(self.z0/40)- retract))
-            self.allezA(maxx, miny,(int(self.z0/40)- retract))
-            self.allezA(minx,miny,(int(self.z0/40)- retract))
+            self.go_to(int(self.x0/40),int(self.y0/40 ),(int(self.z0/40)- retract))
+            self.go_to(minx,miny,(int(self.z0/40)- retract))  # -5 pou eviter que la téte ne fortte 
+            self.go_to(minx,maxy,(int(self.z0/40)- retract))
+            self.go_to(maxx,maxy,(int(self.z0/40)- retract))
+            self.go_to(maxx, miny,(int(self.z0/40)- retract))
+            self.go_to(minx,miny,(int(self.z0/40)- retract))
         return ordre
 
-    def initialisation_connexion(self) -> None:
+    def initialisation_connexion(self) -> str:
         if (self.ser != None ):
             sorti = self._commander_("@07\r")
             print("Initialisation Connection: " , sorti )
@@ -116,33 +119,34 @@ class CNC:
                 sorti= self._commander_("@0R7\r")
                 print("AutoHome :" , sorti )
                 if (sorti == "0"):
-                    self.eta =True
+                    self.state =True
                     self.x , self.y ,self.z = 0,0,0
-                
+                    return "Bien Connecter"
             else:
-                self.eta = False
+                self.state = False
+                return "non disponible"
         else:
-            print("Non inisialisable ")
+            return ("Non inisialisable ")
 
     def deconnection(self) -> None:
         try :
-            self.OutilsStop()
+            self.Stop_Tool()
             self.ser.close()
-            self.eta = False
+            self.state = False
         except:
             print("Erreur Déconection")
 
-    def afficher_instruction(self, instruction, ende="\n") -> None:
+    def show_instruction(self, instruction, ende="\n") -> None:
         instru = instruction.split(',')
         print("X: {:.2f} Y: {:.2f} Z: {:.2f} speed: {:.2f}".format(float(instru[0][4:])/40, float(instru[2])/40, float(instru[4])/40, float(instru[3])/40), end=ende)
 
-    def envoyer_Posistion(self, instruction: str) -> str: # envoyer commede de position 
-        if (self.eta == True):
+    def send_position(self, instruction: str) -> str: # envoyer commede de position 
+        if (self.state == True):
             try:
                 self.ser.write(instruction.encode('utf-8'))
                 instru = instruction.split(',')
                 self.x , self.y , self.z = float(instru[0][4:]) , float(instru[2]) , abs(float(instru[4]))
-                return self.lire_retour_machine()
+                return self.Read_machine_message()
             except serial.SerialException as e:
                 return ("Erreur lors de l'envoi de l'instruction sur le port série :", e)
         else : 
@@ -151,19 +155,19 @@ class CNC:
     def _commander_(self, instruction: str) -> str: #ordre direct sans vérificateion de connection pas de possition
         try:
             self.ser.write(instruction.encode('utf-8'))
-            return self.lire_retour_machine()
+            return self.Read_machine_message()
         except serial.SerialException as e:
             return ("Erreur lors de l'envoi de l'instruction sur le port série :", e)
         
     def commande(self, instruction: str) -> str: #ordre direct sans vérificateion de connection pas de possition
-        if ( self.eta == True):
+        if ( self.state == True):
             self.ser.write(instruction.encode('utf-8'))
-            return self.lire_retour_machine()
+            return self.Read_machine_message()
         else:
             return ("Erreur lors de l'envoi de l'instruction sur le port série :")
         
-    def allezA(self,X:int,Y:int,Z:int) -> str:
-        if (self.eta == True ):
+    def go_to(self,X:int,Y:int,Z:int) -> str:
+        if (self.state == True ):
             if (X < 0 or X > 200):
                 return ("x en dehors du plateau")
             elif (Y < 0 or Y > 150):
@@ -171,11 +175,11 @@ class CNC:
             elif (Z < 0 or Z > 100):
                 return ("z en dehors du plateau")
             else:
-                return self.envoyer_Posistion(f"@0M {X*40},{self.speed},{Y*40},{self.speed},{-abs(Z*40)},{self.speed},{-abs(Z*40)},{self.speed}\r")
+                return self.send_position(f"@0M {X*40},{self.speed},{Y*40},{self.speed},{-abs(Z*40)},{self.speed},{-abs(Z*40)},{self.speed}\r")
         else: print('machine non dispo')
 
-    def allezAMachine(self,x:int,y:int,z:int)-> str: # problemme 
-        if (self.eta == True ):
+    def go_to_machin(self,x:int,y:int,z:int)-> str: # problemme 
+        if (self.state == True ):
             if (x < 0 or x > 8000):
                 return ("x en dehors du plateau")
             elif (y < 0 or y > 6000):
@@ -183,8 +187,8 @@ class CNC:
             elif (z < 0 or z > 4000):
                 return ("z en dehors du plateau")
             else:
-                print(f"@0M {x},{self.speed},{y},{self.speed},{-abs(z)},{self.speed},{-abs(z)},{self.speed}\r")
-                return self.envoyer_Posistion(f"@0M {x},{self.speed},{y},{self.speed},{-abs(z)},{self.speed},{-abs(z)},{self.speed}\r")
+                #print(f"@0M {x},{self.speed},{y},{self.speed},{-abs(z)},{self.speed},{-abs(z)},{self.speed}\r")
+                return self.send_position(f"@0M {x},{self.speed},{y},{self.speed},{-abs(z)},{self.speed},{-abs(z)},{self.speed}\r")
         else: print('machine non dispo')
 
     def SetLocal0(self) -> None:
@@ -193,42 +197,45 @@ class CNC:
         self.z0 = self.z
 
     def AutoHome(self) -> str:
+        self.x = 0
+        self.y = 0
+        self.z = 0
         return self.commande("@0R7\r")
 
-    def DeplacerX(self , X: int) -> str:
-        if ( (self.x + (X*40)) < 0 or (self.x + (X*40)) > (200*40)):
-            return self.allezAMachine((self.x + X*40), self.y , self.z)
+    def move_X(self , X: int) -> str:
+        if ( (self.x + (X*40)) > 0 or (self.x + (X*40)) < 8000):
+            return self.go_to_machin(int(self.x + X*40), int(self.y) , int(self.z))
         else: print("Position x invalide")
         
-    def DeplacerY(self, Y: int) -> str:
-        if (self.y + (Y * 40)) < 0 or (self.y + (Y * 40)) > (150 * 40):
-            return self.allezAMachine(self.x, (self.y + Y), self.z)
+    def move_Y(self, Y: int) -> str:
+        if (self.y + (Y * 40)) > 0 or (self.y + (Y * 40) < 6000):
+            return self.go_to_machin( int(self.x), int(self.y + Y*40),  int(self.z))
         else: print("Position y invalide")
 
-    def DeplacerZ(self, Z: int) -> str:
-        if (self.z + (Z * 40)) < 0 or (self.z + (Z * 40)) > (100 * 40):
-            return self.allezAMachine(self.x, self.y, (self.z + Z ))
+    def move_Z(self, Z: int) -> str:
+        if (self.z + (Z * 40)) > 0 or (self.z + (Z * 40) < 4000):
+            return self.go_to_machin( int(self.x),  int(self.y), int(self.z + Z ))
         else: print("Position Z invalide")
     
-    def DefVitess(self , SPEED : int) -> None: # maj a speed = mm/s pour les ordres de déplacement 
+    def DefSpeed(self , SPEED : int) -> None: # maj a speed = mm/s pour les ordres de déplacement 
         if (SPEED > 0 and SPEED < 50):
             self.speed = SPEED*40
         else:
             print("Vitesse non conforme")
 
-    def OutilsDemarer(self) -> str:
+    def Start_Tool(self) -> str:
         return self.commande("@0B2,1\r")
 
-    def OutilsStop(self)-> str:
+    def Stop_Tool(self)-> str:
         return self.commande("@0B2,0\r")
 
-    def PorteBloquer(self)-> str:
-        return self.commande("@0B1,0\r")
+    def Close_Door(self)-> str:
+        return self.commande("@0B1,0\r") 
 
-    def PorteOuvrire(self)-> str:
+    def Open_Door(self)-> str:
         return self.commande("@0B1,1\r")
 
-    def lire_retour_machine(self) -> str: # retoune la commande de sortie si erreur la met entre /
+    def Read_machine_message(self) -> str: # retoune la commande de sortie si erreur la met entre /
             retour = self.ser.read(1).decode('utf-8')
             #print("Retour machine :", retour)
             if retour == "0" :
@@ -257,22 +264,17 @@ if __name__ == "__main__":
 
     briot.initialisation_connexion()
 
-    briot.DefVitess(20)
+    briot.DefSpeed(35)
 
-    briot.allezA(140,29,31)
-    briot.SetLocal0()
+    briot.go_to(20,20,20)
 
-    gcode = briot.lire_fichier_gcode("Holder.nc")
-    instruction = briot.generer_ordres(gcode)
+    print(briot.move_X(5))
+
+    for i in range(5):
+        print(briot.move_X(3))
+
     
-    
-    briot.OutilsDemarer()
 
-    for i in instruction:
-        briot.afficher_instruction(i, ende="\t")
-        print(briot.envoyer_Posistion(i))
-
-
-    briot.OutilsStop()
+    briot.Stop_Tool()
 
     briot.deconnection()
